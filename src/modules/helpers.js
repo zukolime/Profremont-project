b37fb8b0574dc2b45a448b460da9d11ce7a70031
zukolime <alena.perminova.1993@gmail.com>
2025-03-27 09:49:59 +0500

    add: helpers module

---
+++ src/modules/helpers.js
@@ -0,0 +1,18 @@
+const animate = ({ timing, draw, duration }) => {
+  const start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    const progress = timing(timeFraction);
+
+    draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+  });
+};
+
+export { animate };
